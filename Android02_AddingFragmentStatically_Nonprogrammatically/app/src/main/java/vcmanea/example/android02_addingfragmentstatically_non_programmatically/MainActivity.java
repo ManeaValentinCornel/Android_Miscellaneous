4e09fb9cf97e0894425cdee5ce919e09d2163d96
ManeaValentinCornel <manea.valentin.cornel@gmail.com>
2019-05-06 14:15:14 +0100

    Always remember that you're unique, just like everybody else.
    -course taught by Anil Deshpande on https://www.youtube.com/watch?v=I7yu-5PoteE.

---
+++ Android02_AddingFragmentStatically_Nonprogrammatically/app/src/main/java/vcmanea/example/android02_addingfragmentstatically_non_programmatically/MainActivity.java
@@ -0,0 +1,13 @@
+package vcmanea.example.android02_addingfragmentstatically_non_programmatically;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Android02_AddingFragmentStatically_Nonprogrammatically/app/src/test/java/vcmanea/example/android02_addingfragmentstatically_non_programmatically/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package vcmanea.example.android02_addingfragmentstatically_non_programmatically;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
